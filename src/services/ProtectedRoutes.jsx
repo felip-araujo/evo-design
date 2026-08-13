55c605b45aba942d795feaac7336a3fb593f8a23
felip-araujo <felipearaujoexecutive@gmail.com>
2026-08-13 16:08:14 -0400

    again

--- src/services/ProtectedRoutes.jsx
+++ src/services/ProtectedRoutes.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { isAuthenticated, getAccount } from "./Auth.jsx";
+import { isAuthenticated, getAccount } from "./Auth";
 
 
 export function ProtectedRoute({ children, allowedRoles, allowedTypes }) {
